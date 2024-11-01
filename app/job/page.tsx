745a8f4e6643b4b2814648b78edbd23a1bca0f1b
Đoàn Trí Hùng <koikoidth12@gmail.com>
2024-11-02 02:25:25 +0700

    third commit

--- app/job/page.tsx
+++ app/job/page.tsx
@@ -1,18 +1,23 @@
-"use client"
-import SearchBar from '@/components/Search/SearchBar'
-import React from 'react'
+"use client";
+import JobCard from "@/components/Folder Components/JobCard/JobCard";
+import SearchBar from "@/components/Search/SearchBar";
+import React from "react";
+import { InfoJob } from "@/lib/data";
+import { JobPost } from "@/lib/interface";
+import Link from "next/link";
 
 export default function JobPage() {
   return (
-
-    <div className='bg-custom h-[100vh] '>
+    <div className="pb-10">
       <div className=" mx-[15%]  pt-[60px]">
         <SearchBar></SearchBar>
       </div>
-      <div >
 
+      <div className="text-center mt-[50px] mx-[12%] gap-[50px]  grid grid-cols-3 ">
+        {
+          InfoJob.map((job: JobPost) => <JobCard job = {job} key={job.id}/>)
+        }
       </div>
     </div>
-
-  )
-}
+  );
+};
