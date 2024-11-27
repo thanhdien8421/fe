076bc463b981291f815c96354b33e6486bda179d
HieuHoang83 <139992556+HieuHoang83@users.noreply.github.com>
2024-11-27 21:28:48 +0700

    ok login

--- action/login.ts
+++ action/login.ts
@@ -3,19 +3,48 @@ import { LoginSchema } from "@/schema/LoginSchema";
 
 // Helper function to delay with a Promise
 function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Make UploadCV an async function
 export async function Login(values: z.infer<typeof LoginSchema>) {
   // Wait for 2 seconds
-  await delay(2000);
 
   // Log the values after the delay
-  console.log(values);
-
-  return {
-    message: "Đăng nhập thành công",
-    success: true,
+  const url = "http://localhost:8000/api/v1/auth/login";
+  const data = {
+    username: values.email,
+    password: values.password,
+    type: values.role,
   };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+
+    const result = await response.json();
+    if (result?.response?.error) {
+      throw new Error(result?.response?.error);
+    }
+    return {
+      message: "Đăng nhập thành công",
+      success: true,
+      user: result?.data?.user,
+    };
+  } catch (error) {
+    return {
+      message: "Tài khoản hoặc mât khẩu không chính xác",
+      success: true,
+      user: null,
+    };
+  }
 }
