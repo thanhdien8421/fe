32b434141d82f9e69074c32ecc0cc476a322de90
Enigmask22 <superxayan2@gmail.com>
2024-12-13 19:42:50 +0700

    3

--- components/NavigationBar/NavBar.tsx
+++ components/NavigationBar/NavBar.tsx
@@ -91,7 +91,7 @@ const NavBar = () => {
                 </button>
               </Link>
               <Link href="/signup">
-                <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-sky-600 text-white h-9 px-4 py-2 hover:bg-sky-700">
+                <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-sky-600 text-sky-600 h-9 px-4 py-2 hover:bg-sky-600 hover:text-white">
                   Đăng ký
                 </button>
               </Link>
